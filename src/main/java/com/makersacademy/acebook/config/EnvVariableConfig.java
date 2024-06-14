1ab7cab990be4bd263f70df331d9cd8ae58a638f
Makini <hakim.kayira@gmail.com>
2024-06-14 11:19:15 +0100

    Refactored workflow yaml file

---
+++ src/main/java/com/makersacademy/acebook/config/EnvVariableConfig.java
@@ -0,0 +1,21 @@
+//package com.makersacademy.acebook.config;
+//
+//import lombok.Getter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Getter
+//@Configuration
+//public class EnvVariableConfig {
+//
+//    // Getter methods to use these values wherever needed
+//    @Value("${local_password}")
+//    private String localPassword;
+//
+//    @Value("${local_db}")
+//    private String localDbUrl;
+//
+//    @Value("${local_db_test}")
+//    private String local_test_url;
+//
+//}
